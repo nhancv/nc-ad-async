1170fbe1acaac2a6c270fe199146a2c9c70f02f4
Nhan Cao <nhancv1992@gmail.com>
2017-02-08 22:48:15 +0700

    Async: thread and handler

--- app/src/main/java/com/nhancv/async/MainActivity.java
+++ app/src/main/java/com/nhancv/async/MainActivity.java
@@ -1,5 +1,8 @@
 package com.nhancv.async;
 
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -14,8 +17,31 @@ public class MainActivity extends AppCompatActivity {
     @ViewById(R.id.tvMsg)
     TextView tvMsg;
 
+    private Handler handler;
+
     @AfterViews
     void init() {
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                String tickMsg = msg.obj.toString();
+                tvMsg.setText(tickMsg);
+            }
+        };
+
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                //get message from MainThread
+                Message msg = handler.obtainMessage();
+                //update message
+                msg.obj = "Tick  " + i;
+                //return message back to MainThread
+                handler.sendMessage(msg);
+                SystemClock.sleep(1000);
+            }
+        }).start();
 
     }
 
