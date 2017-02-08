b3371a6580025f9df804ab9897962b1e78ad2244
Nhan Cao <nhancv1992@gmail.com>
2017-02-09 01:16:47 +0700

    RxAndroid: advance with retrofit

--- app/src/main/java/com/nhancv/async/MainActivity.java
+++ app/src/main/java/com/nhancv/async/MainActivity.java
@@ -3,9 +3,22 @@ package com.nhancv.async;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.google.gson.annotations.SerializedName;
+
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.List;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +29,39 @@ public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     void init() {
-
+        fetchGithub();
     }
+
+    private void updateText(String tick) {
+        tvMsg.setText(tick);
+    }
+
+    private void fetchGithub() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GitHubService service = retrofit.create(GitHubService.class);
+
+        Observable.defer(() -> service.listRepos("nhancv"))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(repoList -> {
+                    updateText("Done: " + repoList.size());
+                });
+    }
+
+    public interface GitHubService {
+        @GET("users/{user}/repos")
+        Observable<List<Repo>> listRepos(@Path("user") String user);
+    }
+
+    class Repo {
+        @SerializedName("id")
+        String id;
+    }
+
 
 
 }
