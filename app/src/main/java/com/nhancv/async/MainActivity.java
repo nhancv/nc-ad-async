6ce2add7ccedd86898b131d0aae278cda60d0e30
Nhan Cao <nhancv1992@gmail.com>
2017-02-08 22:31:07 +0700

    init common libs

--- app/src/main/java/com/nhancv/async/MainActivity.java
+++ app/src/main/java/com/nhancv/async/MainActivity.java
@@ -1,13 +1,23 @@
 package com.nhancv.async;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @ViewById(R.id.tvMsg)
+    TextView tvMsg;
+
+    @AfterViews
+    void init() {
+
     }
+
+
 }
