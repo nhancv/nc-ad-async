489f67ce9c5ab98d41591f8999cbb1d6a5637179
Nhan Cao <nhancv1992@gmail.com>
2017-02-08 23:49:36 +0700

    Async: AsyncTask

--- app/src/main/java/com/nhancv/async/MainActivity.java
+++ app/src/main/java/com/nhancv/async/MainActivity.java
@@ -1,11 +1,15 @@
 package com.nhancv.async;
 
+import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.Random;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +20,39 @@ public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     void init() {
-
+        new TickWithAsyncTask().execute();
     }
 
+    private void updateText(String tick) {
+        tvMsg.setText(tick);
+    }
+
+    private class TickWithAsyncTask extends AsyncTask<Void, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            updateText("Start");
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            //Delay random for test execute tasks in parallel
+            SystemClock.sleep(Math.abs(new Random().nextInt(3000)));
+            for (int i = 0; i < 5; i++) {
+                publishProgress(i);
+                SystemClock.sleep(1000);
+            }
+            return "Done";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            updateText(String.valueOf(progress[0]));
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            updateText(result);
+        }
+    }
 
 }
