ae3433f5e53a354671cc7e6413ba707158a677de
Nhan Cao <nhancv1992@gmail.com>
2017-02-09 01:27:10 +0700

    RxAndroid: rxsample

--- app/src/main/java/com/nhancv/async/MainActivity.java
+++ app/src/main/java/com/nhancv/async/MainActivity.java
@@ -3,19 +3,10 @@ package com.nhancv.async;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.google.gson.annotations.SerializedName;
-
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
-import java.util.List;
-
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -29,39 +20,24 @@ public class MainActivity extends AppCompatActivity {
 
     @AfterViews
     void init() {
-        fetchGithub();
+        rxSample();
     }
 
     private void updateText(String tick) {
         tvMsg.setText(tick);
     }
 
-    private void fetchGithub() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        GitHubService service = retrofit.create(GitHubService.class);
+    private void rxSample() {
 
-        Observable.defer(() -> service.listRepos("nhancv"))
+        Observable.defer(() -> Observable.just(1, 2, 3, 4, 5, 6))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(repoList -> {
-                    updateText("Done: " + repoList.size());
+                .filter(integer -> integer > 4) //5, 6
+                .map(integer -> integer * 2) //10, 12
+                .flatMap(integer -> Observable.range(integer, 3)) // 10, 11, 12, 12, 13, 14
+                .subscribe(integer -> {
+                    updateText(tvMsg.getText() + " " + integer);
                 });
     }
-
-    public interface GitHubService {
-        @GET("users/{user}/repos")
-        Observable<List<Repo>> listRepos(@Path("user") String user);
-    }
-
-    class Repo {
-        @SerializedName("id")
-        String id;
-    }
-
-
 
 }
