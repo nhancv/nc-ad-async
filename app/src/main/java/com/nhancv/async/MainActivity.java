dd889eba96143d08a870c84aa9d71a0a3d536463
Nhan Cao <nhancv92@gmail.com>
2017-03-13 08:50:00 +0700

    integrate rxjava math, example error interceptor

--- app/src/main/java/com/nhancv/async/MainActivity.java
+++ app/src/main/java/com/nhancv/async/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nhancv.async;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
@@ -38,14 +39,44 @@ public class MainActivity extends AppCompatActivity {
     private void rxSample() {
 
         Observable.defer(() -> Observable.just(1, 2, 3, 4, 5, 6))
-                .subscribeOn(Schedulers.io())
+                .flatMap(integer -> {
+                    Log.e(TAG, "rxSample: emit error 0");
+                    return Observable.error(new Exception("error 0"));
+                })
+                .doOnSubscribe(() -> {
+                    Log.e(TAG, "rxSample: begin");
+                })
+                .doOnError(throwable -> {
+                    Log.e(TAG, "rxSample: do on error 1 " + throwable);
+                })
+                .doOnNext(integer -> {
+                    Log.e(TAG, "rxSample: next " + integer);
+                })
+                .doOnCompleted(() -> {
+                    Log.e(TAG, "rxSample: completed");
+                })
+                .doOnTerminate(() -> {
+                    Log.e(TAG, "rxSample: terminate");
+                })
+                .onErrorResumeNext(throwable -> {
+                    Log.e(TAG, "rxSample: emit error 1");
+                    return Observable.error(new Exception("error 1"));
+                })
+                .doOnError(throwable -> {
+                    Log.e(TAG, "rxSample: do on error 2 " + throwable);
+                })
+                .onErrorResumeNext(throwable -> {
+                    Log.e(TAG, "rxSample: emit error 2");
+                    return Observable.error(new Exception("error 2"));
+                })
+                .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
-                .filter(integer -> integer > 4) //5, 6
-                .map(integer -> integer * 2) //10, 12
-                .flatMap(integer -> Observable.range(integer, 3)) // 10, 11, 12, 12, 13, 14
                 .subscribe(integer -> {
-                    updateText(tvMsg.getText() + " " + integer);
+                    Log.e(TAG, "rxSample: " + integer);
+                }, throwable -> {
+                    Log.e(TAG, "rxSample: error final " + throwable);
                 });
+
     }
 
 }
